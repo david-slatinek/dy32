7fdcee78ee55ca3a871488c2303eff20ba1b12dd
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-12-22 21:16:35 +0100

    Add a function to generate random customer

---
+++ kafka-producer/random/customer.go
@@ -0,0 +1,18 @@
+package random
+
+import (
+	"github.com/google/uuid"
+	"kafka-producer/model"
+)
+
+func Customer() model.Info {
+	return model.Customer{
+		ID:           uuid.NewString(),
+		Name:         String(5, 10),
+		Lastname:     String(5, 15),
+		StreetName:   String(5, 8),
+		StreetNumber: String(1, 3),
+		PostNumber:   Int(1000, 9000),
+		PostTitle:    String(5, 10),
+	}
+}
