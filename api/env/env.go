7ad5e1d6d3ade4876681244907c92642d703ea2e
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2023-01-02 15:52:24 +0100

    Add env

---
+++ api/env/env.go
@@ -0,0 +1,7 @@
+package env
+
+import "github.com/joho/godotenv"
+
+func Load(fileName string) error {
+	return godotenv.Load(fileName)
+}
--- api/main.go
+++ api/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"main/env"
+	"os"
+)
 
+func main() {
+	err := env.Load("env/.env")
+	if err != nil {
+		log.Fatalf("error with env.Load: %v", err)
+	}
+
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("MONGODB_URI is not set")
+	}
 }
